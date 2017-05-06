4dd68c5f5abf66a3ce95a273fdc4e2065c9aac27
davidkoo7 <david@uon.edu.au>
2017-05-07 01:18:13 +0800

    Master

---
+++ Whoopee/GamePage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Whoopee
+{
+    [Activity(Label = "GamePage", Theme = "@android:style/Theme.Black.NoTitleBar.Fullscreen")]
+    public class GamePage : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application he
+            SetContentView(Resource.Layout.GamePage);
+
+            Random r = new Random();
+            TextView firstNum;
+            TextView secondNum;
+
+            Button btnNum1;
+            Button btnNum2;
+            Button btnNum3;
+
+            TextView tvResult;
+            
+            firstNum = FindViewById<TextView>(Resource.Id.tvFirstNum);
+            secondNum = FindViewById<TextView>(Resource.Id.tvSecondNum);
+
+            btnNum1 = FindViewById<Button>(Resource.Id.btnFirstNum);
+            btnNum2 = FindViewById<Button>(Resource.Id.btnSecondNum);
+            btnNum3 = FindViewById<Button>(Resource.Id.btnThirdNum);
+
+            tvResult = FindViewById<TextView>(Resource.Id.tvResult);
+
+            firstNum.Text = r.Next(1, 10).ToString();
+            secondNum.Text = r.Next(11, 20).ToString();
+
+            btnNum1.Text = (Convert.ToInt32(firstNum.Text) + Convert.ToInt32(secondNum.Text) + 5 ).ToString();
+            btnNum2.Text = (Convert.ToInt32(firstNum.Text) + Convert.ToInt32(secondNum.Text)).ToString();
+            btnNum3.Text = (Convert.ToInt32(firstNum.Text) + Convert.ToInt32(secondNum.Text) - 5).ToString();
+        }
+    }
+}
---
+++ Whoopee/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Whoopee
+{
+    [Activity(Label = "Whoopee", MainLauncher = true, Icon = "@drawable/icon", Theme = "@android:style/Theme.Black.NoTitleBar.Fullscreen")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            //Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            //button.Click += delegatebt { button.Text = string.Format("{0} clicks!", count++); };
+
+            Button btnNewGame;
+
+            btnNewGame = FindViewById<Button>(Resource.Id.btnNewGame);
+            btnNewGame.Click += BtnNewGame_Click;
+        }
+
+        private void BtnNewGame_Click(object sender, EventArgs e)
+        {
+            Intent nextActivity = new Intent(this, typeof(GamePage));
+            StartActivity(nextActivity);
+        }
+    }
+}
+
